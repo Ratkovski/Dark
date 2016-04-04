a30e1b48bda07238a5f25d6d94044430448b14d1
Ratkovski <drezcry@hotmail.com>
2016-04-04 14:40:13 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,592 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Sistemas de Informação</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="justified-nav.css" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="css/main.css" /
+       <link rel="stylesheet" type="text/css" href="css/fontello.css" />
+
+    <link rel="stylesheet" href="css/styles.css">
+    
+    
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" media="screen" href="css/styles.min.css" />
+		<link rel="stylesheet" media="screen" href="css/responsive.min.css" />
+        <link href="css/scrolling-nav.css" rel="stylesheet">
+
+        
+      
+		<link rel="stylesheet" href="css/calendar.css">
+
+        
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+     /* CUSTOMIZE THE NAVBAR
+    -------------------------------------------------- */
+
+    /* Carousel base class */
+    .carousel {
+      margin-bottom: 60px;
+    }
+
+    .carousel .container {
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+
+    .carousel-control {
+      background-color: transparent;
+      border: 0;
+      font-size: 120px;
+      margin-top: 0;
+      text-shadow: 0 1px 1px rgba(0,0,0,.4);
+    }
+
+    .carousel .item {
+      height: 500px;
+    }
+    .carousel img {
+      min-width: 100%;
+      height: 500px;
+    }
+
+    .carousel-caption {
+      background-color: transparent;
+      position: static;
+      max-width: 550px;
+      padding: 0 20px;
+      margin-bottom: 100px;
+    }
+    .carousel-caption h1,
+    .carousel-caption .lead {
+      margin: 0;
+      line-height: 1.25;
+      color: #fff;
+      text-shadow: 0 1px 1px rgba(0,0,0,.4);
+    }
+    .carousel-caption .btn {
+      margin-top: 10px;
+    }
+
+
+
+    /* MARKETING CONTENT
+    -------------------------------------------------- */
+
+    /* Center align the text within the three columns below the carousel */
+    .marketing .span4 {
+      text-align: center;
+    }
+    .marketing h2 {
+      font-weight: normal;
+    }
+    .marketing .span4 p {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+
+
+    /* Featurettes
+    ------------------------- */
+
+    .featurette-divider {
+      margin: 80px 0; /* Space out the Bootstrap <hr> more */
+    }
+    .featurette {
+      padding-top: 120px; /* Vertically center images part 1: add padding above and below text. */
+      overflow: hidden; /* Vertically center images part 2: clear their floats. */
+    }
+    .featurette-image {
+      margin-top: -120px; /* Vertically center images part 3: negative margin up the image the same amount of the padding to center it. */
+    }
+
+    /* Give some space on the sides of the floated elements so text doesn't run right into it. */
+    .featurette-image.pull-left {
+      margin-right: 40px;
+    }
+    .featurette-image.pull-right {
+      margin-left: 40px;
+    }
+
+    /* Thin out the marketing headings */
+    .featurette-heading {
+      font-size: 50px;
+      font-weight: 300;
+      line-height: 1;
+      letter-spacing: -1px;
+    }
+
+
+
+    /* RESPONSIVE CSS
+    -------------------------------------------------- */
+
+    @media (max-width: 979px) {
+
+      .container.navbar-wrapper {
+        margin-bottom: 0;
+        width: auto;
+      }
+      .navbar-inner {
+        border-radius: 0;
+        margin: -20px 0;
+      }
+
+      .carousel .item {
+        height: 500px;
+      }
+      .carousel img {
+        width: auto;
+        height: 500px;
+      }
+
+      .featurette {
+        height: auto;
+        padding: 0;
+      }
+      .featurette-image.pull-left,
+      .featurette-image.pull-right {
+        display: block;
+        float: none;
+        max-width: 40%;
+        margin: 0 auto 20px;
+      }
+    }
+
+
+    @media (max-width: 767px) {
+
+      .navbar-inner {
+        margin: -20px;
+      }
+
+      .carousel {
+        margin-left: -20px;
+        margin-right: -20px;
+      }
+      .carousel .container {
+
+      }
+      .carousel .item {
+        height: 300px;
+      }
+      .carousel img {
+        height: 300px;
+      }
+      .carousel-caption {
+        width: 65%;
+        padding: 0 70px;
+        margin-bottom: 40px;
+      }
+      .carousel-caption h1 {
+        font-size: 30px;
+      }
+      .carousel-caption .lead,
+      .carousel-caption .btn {
+        font-size: 18px;
+      }
+
+      .marketing .span4 + .span4 {
+        margin-top: 40px;
+      }
+
+      .featurette-heading {
+        font-size: 30px;
+      }
+      .featurette .lead {
+        font-size: 18px;
+        line-height: 1.5;
+      }
+	  
+
+
+
+
+    </style>
+    
+    
+    
+    
+    
+
+  </head>
+
+  <body  id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+  
+  <body>
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" style="margin-top:90px">
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="img/slide-01.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Sistemas de Informa&ccedil;&atilde;o</h1>
+              <p class="lead">Conhe&ccedil;a a carreira em Sistemas de Informa&ccedil;&atilde;o, como est&aacute; o mercado de trabalho para este profissional e saiba onde cursar esta gradua&ccedil;&atilde;o! </p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/slide-02.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Melhores faculdades</h1>
+              <p class="lead">S&atilde;o mais de 360 as faculdades autorizadas a oferecer o curso de Sistemas de Informa&ccedil;&atilde;o no Pa&iacute;s, nas modalidades presencial ou a dist&acirc;ncia.</p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/slide-03.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Intuito</h1>
+              <p class="lead">Desenvolvimento e manuten&ccedil;&atilde;o de sistemas de informa&ccedil;&atilde;o voltados &agrave; administra&ccedil;&atilde;o dos dados organizacionais, capturando, armazenando e tratando-os segundo os crit&eacute;rios estabelecidos pelos processos vigentes na organiza&ccedil;&atilde;o, como ferramenta de apoio &agrave;s &aacute;reas operacional, t&aacute;tica e estrat&eacute;gica.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div><!-- /.carousel -->
+  
+  
+  
+  <section id="navigation">
+<nav class="navbar navbar-default navbar-fixed-top " role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse menu">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li>
+                 		<a class="navbar-brand page-scroll" href="#myCarousel">Home</a>       
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#functions">Notícias</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Calendário</a>
+                    </li>
+                 
+                    <li>
+                        <a class="page-scroll" href="#footer">Contato</a>
+                    </li>
+                        <li>
+                        <a class="page-scroll" href="#cases">Horários</a>
+                    </li>
+                   
+                    <li>
+                        <a class="page-scroll" href="others/ementa.php">Ementa</a>
+                    </li>
+                     <li>
+                        <a class="page-scroll" href="others/sobre.php">Sobre</a>
+                    </li>
+                    
+                   
+               
+      
+            </li
+                ></ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    </section>
+
+    
+    
+        
+		<section id="functions">
+
+			<div class="container text-center">
+
+				<div class="row">
+					<div class="col-md-12">
+						<h1>Feed de Notícias!</h1>
+					</div>
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-4 feature">
+						<img src="Images/n4.jpg" class="show-video">
+						<h4>Faltam profissionais de sistemas de informação no país</h4>
+						<p>Em médio prazo, número de mão-de-obra necessária pode chegar a três milhões.
+Crescimento da área de tecnologia da informação é estimado em 10% ao ano.</p>
+						<p><a class="btn btn-primary" href="notices/n1.php" role="button">Leia mais... &raquo;</a></p>
+					</div>
+                    
+                    <div class="col-md-4 feature">
+						<img src="Images/n4.jpg">
+						<h4>Ciência da Computação, Sistemas de Informação ou Engenharia da Computação?</h4>
+						<p>Ciência da Computação, Sistemas de Informação ou Engenharia da Computação? Qualquer semelhança "não é" mera coincidência. Os 3 cursos de bacharelado em Tecnologia da Informação licenciados pelo Ministério da Educação têm nomes tão similares que até rimam. Mais do que isso, aproximadamente 30% do conteúdo é praticamente o mesmo.</p>
+						<p><a class="btn btn-primary" href="notices/n2.php" role="button">Leia mais... &raquo;</a></p>
+					</div>
+                    
+                    <div class="col-md-4 feature">
+						<img src="Images/n3.jpg">
+						<h4>Passou da hora do profissional de TI ser reconhecido nacionalmente.</h4>
+						<p>O profissional do século 21? - Hoje em dia uma criança pode usar celular e computador facilmente, mas quando se pergunta o que quer ser quando crescer, pode responder tudo menos que quer ser um profissional de TI</p>
+						<p><a class="btn btn-primary" href="notices/n4.php" role="button">Leia mais... &raquo;</a></p>
+					</div>
+
+					
+				<div class="row">
+
+					<div class="col-md-12">
+						<a href="notices/noticias.php" class="btn btn-lg btn-blue margin-top btn-more">Veja mais</a>
+					</div>
+
+				</div>
+                </div>
+                </section>
+                
+                
+          <section class="container">
+	
+
+
+                <div id="services" class="page-header">
+            
+                    <div class="pull-right form-inline">
+                        <div class="btn-group">
+                            <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
+                            <button class="btn" data-calendar-nav="today">Today</button>
+                            <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+                        </div>
+                        <div class="btn-group">
+                            <button class="btn btn-warning" data-calendar-view="year">Year</button>
+                            <button class="btn btn-warning active" data-calendar-view="month">Month</button>
+                            <button class="btn btn-warning" data-calendar-view="week">Week</button>
+                            <button class="btn btn-warning" data-calendar-view="day">Day</button>
+                        </div>
+                    </div>
+            
+                    <h3></h3>	
+                </div>
+
+                <div class="row">
+                    <div class="calendario">
+
+                        <div id="calendar"></div>
+                    </div>
+                    <div class="span3">
+                        <div class="row-fluid">
+                            <select id="language" class="span12">
+                                <option value="">Select Language (default: pt-BR)</option>
+                                <option value="pt-BR">Portuguese (Brazil)</option>
+                                <option value="es-ES">Spanish (Spain)</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                
+         </section>
+         
+         <div class="container">
+
+ <center> <h2 >Horários</h2></center>  
+  <section  id="cases"> 
+        
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Horário:</th>
+        <th>Segunda-Feira:</th>
+        <th>Terça-feira:</th>
+		<th>Quarta-feira:</th>
+		<th>Quinta-feira:</th>
+		<th>Sexta-feira:</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>19:30/19:50</td>
+        <td>Gerência de Software</td>
+        <td>Administração e Gerência de Rede</td>
+		<td>Programação Web</td>
+		<td>Comportamento Organizacional</td>
+		<td>Banco de Dados III</td>
+      </tr>
+      <tr>
+        <td>19:50/20:40</td>
+        <td>Gerência de Software</td>
+        <td>Administração e Gerência de Rede</td>
+		<td>Programação Web</td>
+		<td>Comportamento Organizacional</td>
+		<td>Banco de Dados III</td>
+      </tr>
+      <tr>
+        <td>20:40/20:55</td>
+        <td>Intervalo</td>
+		<td>Intervalo</td>
+		<td>Intervalo</td>
+		<td>Intervalo</td>
+		<td>Intervalo</td>
+      </tr>
+	  <tr>
+        <td>20:55/21:45</td>
+        <td>Gerência de Software</td>
+        <td>Administração e Gerência de Rede</td>
+		<td>Programação Web</td>
+		<td>Comportamento Organizacional</td>
+		<td>Banco de Dados III</td>
+      </tr>
+	  <tr>
+        <td>21:45/22:35</td>
+        <td>Gerência de Software</td>
+        <td>Administração e Gerência de Rede</td>
+		<td>Programação Web</td>
+		<td>Comportamento Organizacional</td>
+		<td>Banco de Dados III</td>
+      </tr>
+    </tbody>
+  </table>
+
+</section>
+
+
+      <!-- Site footer -->
+     <footer id="footer">
+
+			<div class="container">
+
+	<div class="row links">
+    
+    		<div class="col-lg-3 col-md-4 col-sm-4 hidden-xs no-padding padding-bottom text-left">
+				<ul>
+					<li class="title">Hand5</li>
+					<li><a href="others/sobre.php">Sobre</a></li>
+					<li><a href="others/ementa.php">Ementa</a></li>
+					<li><a href="notices/noticias.php">Notícias</a></li>
+                     <li class="glyphicon-menu-down"><a href="#">Materiais</a>
+                <ul>
+      <li><a href="Aulas/Programacaoweb.html">Programação Web</a></li>
+      <li><a href="Aulas/Comportamentoorganizaciona.html">Comportamento Organizacional</a></li>
+      <li><a href="Aulas/BDIII.html">Banco de DadosIII</a></li>   
+      <li><a href="Aulas/GerenciaprojetosSW.html">Gerencia de Projetos de Software</a></li>  
+      <li><a href="Aulas/ADMgerenciaderedes.html">Administração e Gerência de Redes de  Computadores</a></li>  		 
+                </ul>
+                	</li>
+				</ul>
+			</div>
+            
+            <div class="col-lg-3 col-md-4 col-sm-4 hidden-xs no-padding padding-bottom text-left">
+				<ul>
+					<li class="title">Contato</li>
+					<li><a href="">facebook.com/hand5</a></li>
+					<li><a href="">administrativo@hand5.com</a></li>
+                    <li><a href="">(47)3645-5555</a></li>
+				</ul>
+			</div>
+
+		
+
+		</div>
+        
+        <div class="row socials padding-bottom">
+
+		<div class="col-lg-12">
+			<hr>
+		</div>
+
+		<div class="span3"><center>
+							<a href="#home"><img src="images/logo.png" style="margin-top: 6px;"/></a>
+                            <p>Hand5 © 2015</p></center>
+						</div>
+
+		
+
+	</div>
+
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    
+        <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    
+	<script src="js/scrolling-nav.js"></script>
+    
+    	<script type="text/javascript" src="components/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="components/underscore/underscore-min.js"></script>
+	<script type="text/javascript" src="components/bootstrap2/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="components/jstimezonedetect/jstz.min.js"></script>
+	<script type="text/javascript" src="js/language/pt-BR.js"></script>
+	<script type="text/javascript" src="js/language/es-ES.js"></script>
+	<script type="text/javascript" src="js/calendar.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+
+	<script type="text/javascript">
+		var disqus_shortname = 'bootstrapcalendar'; // required: replace example with your forum shortname
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+	</script>
+    
+    
+        <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery2.js"></script>
+    <script src="js/bootstrap.min2.js"
+    <script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+  </body>
+</html>
